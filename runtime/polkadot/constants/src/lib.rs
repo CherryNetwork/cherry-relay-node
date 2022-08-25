4df0683aedc9ea914880d9e25df4d8db139e2ca9
Fotis Zikos <fzikos@gmail.com>
2022-08-25 12:53:21 +0300

    add: UNITS currency for testing

--- runtime/polkadot/constants/src/lib.rs
+++ runtime/polkadot/constants/src/lib.rs
@@ -27,6 +27,7 @@ pub mod currency {
 	/// The existential deposit.
 	pub const EXISTENTIAL_DEPOSIT: Balance = 100 * CENTS;
 
+	pub const UNITS: Balance = 1_000_000_000_000;
 	pub const MILLICENTS: Balance = 1_000_000_000;
 	pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
 	pub const DOLLARS: Balance = 100 * CENTS;
