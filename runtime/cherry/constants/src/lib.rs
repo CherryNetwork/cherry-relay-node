5920a26554fbd7d32056aa541992c3ce568bfb04
Fotis Zikos <fzikos@gmail.com>
2022-10-27 17:39:01 +0300

    fix: initial balances and token decimals

--- runtime/cherry/constants/src/lib.rs
+++ runtime/cherry/constants/src/lib.rs
@@ -28,9 +28,9 @@ pub mod currency {
 	pub const EXISTENTIAL_DEPOSIT: Balance = 100 * CENTS;
 
 	pub const UNITS: Balance = 1_000_000_000_000_000_000;
-	pub const MILLICENTS: Balance = 1_000_000_000_000;
+	pub const MILLICENTS: Balance = 10_000_000_000_000;
 	pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
-	pub const DOLLARS: Balance = 1_000 * CENTS;
+	pub const DOLLARS: Balance = 100 * CENTS;
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
 		items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
