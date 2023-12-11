f372a12b8ecfd888629ff15017a40afe529cc3f4
Fotis Zikos <fzikos@gmail.com>
2023-12-11 15:06:49 +0200

    bump spec version

--- runtime/cherry/src/lib.rs
+++ runtime/cherry/src/lib.rs
@@ -120,13 +120,13 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("cherry"),
 	impl_name: create_runtime_str!("cherry"),
 	authoring_version: 0,
-	spec_version: 24,
+	spec_version: 25,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 20,
+	transaction_version: 21,
 	state_version: 0,
 };
 
